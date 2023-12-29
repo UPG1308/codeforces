3b46e664156605ffebb2c0ac6e5720b123f2f65a
UDAYKUMAR P G <pguday4@gmail.com>
2023-12-29 21:21:01 +0530

    Create A - Odd One Out.java

---
+++ Codeforces Round 918 (Div. 4)/A - Odd One Out.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      while(t-- > 0){
+        // int n = sc.nextInt();
+        int xor = 0;
+        for(int i = 0; i < 3 ; i++) xor ^= sc.nextInt();
+        System.out.println(xor);
+      }
+  }
+}
